900b31d84ce3092b7f20e785e3f7bfe9381f4d25
cymc0012 <30420029+cymc0012@users.noreply.github.com>
2017-11-01 08:34:30 -0700

    Add files via upload

---
+++ leapYearReturn.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class leapYearReturn {
+	public static boolean isLeapYear(int year) {
+		if((year%4==0)||(year%400==0)) {
+			if(year%100 != 0) {
+				return true;
+			}
+			else if(year%100==0 && year%400!= 0) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		Scanner abx = new Scanner(System.in);
+		System.out.print("Enter a year to see if it is leap year: ");
+		int yr = abx.nextInt();
+		if (isLeapYear(yr)==true) {
+			System.out.println("It is a leap year.");
+		}
+		else { 
+			System.out.println("It is not a leap year.");
+		}
+	}
+}
